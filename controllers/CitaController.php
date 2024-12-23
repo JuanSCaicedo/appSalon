36e4d7f62edefcae560751205d2f1f6245bc2d15
JuanSCaicedo <sebanchit@gmail.com>
2024-12-23 01:30:53 -0500

    modificancando controlador de cita

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -1,20 +1,33 @@
 <?php
- 
+
 namespace Controllers;
- 
+
 use MVC\Router;
- 
-class CitaController{
-    public static function index(Router $router){
-        if(!isset($_SESSION)){
+
+class CitaController
+{
+    public static function index(Router $router)
+    {
+        if (!isset($_SESSION)) {
             session_start();
         }
 
         isAuth();
- 
-        $router->render("cita/index",[
-            "nombre" => $_SESSION["nombre"],
-            "id" => $_SESSION["id"]
+
+        // Verificar si las claves existen en $_SESSION
+        $nombre = isset($_SESSION["nombre"]) ? $_SESSION["nombre"] : null;
+        $id = isset($_SESSION["id"]) ? $_SESSION["id"] : null;
+
+        // Puedes manejar casos donde los valores no estén definidos
+        if (!$nombre || !$id) {
+            // Ejemplo: redirigir al usuario al login
+            header("Location: /login");
+            exit;
+        }
+
+        $router->render("cita/index", [
+            "nombre" => $nombre,
+            "id" => $id
         ]);
     }
 }
